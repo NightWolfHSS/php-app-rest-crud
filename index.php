a09289628016336c85077e387746308f8151682a
Viacheslav <nice.kirat@mail.ru>
2023-03-31 01:48:41 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>php-template </title>
+	<link rel="stylesheet" href="/root/css/main.css">
+</head>
+<body>
+	<header>
+		<div class="linespace"></div>
+		<div class="contain-header">
+			<h1>connecting to database in REST API + CRUD SYSTEMS</h1>
+	    	<p>fast response system | система быстрого реагирования</p>
+		</div>
+
+	</header>
+	<div class="content">
+		
+	</div>
+	<footer>
+		
+	</footer>
+</body>
+</html>
+<!-- 
+создать документацию по установки на локалку или опенсервер
+документация по созданию данных по умолчанию 
+документация по установки данных и подключение к бд
+возможно уже будет включена по умолчанию
+ -->
