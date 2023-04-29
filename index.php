794511f799f162e310769ea8bcbe53a4592a82cc
Viacheslav <nice.kirat@mail.ru>
2023-04-29 20:00:56 +0600

    new files js and add blocks html and css

--- index.php
+++ index.php
@@ -13,24 +13,34 @@
 			<h1>connecting to database in REST API + CRUD SYSTEMS</h1>
 	    	<p>fast response system | система быстрого реагирования</p>
 		</div>
-<br><hr><br>
+		<br><hr><br>
 		<div class="contain-header-x">
 			<h2>интерессный сайт по добовлению какого то контента</h2>
 	    	<p>добовлять можно всякие вещи</p>
 		</div>
-
 	</header>
+
+    <div class="section">
+    	<section class="mob-i">
+	    <h2>Хороший язык : </h2>
+		   <p>PHP это очень хороший язык - для работы с протоколами http / https ... в данном случае мы будем использовать методы для работы с данными <br>
+		   	<b><span title="данный метод используется для добовления данных">POST</span> | <span title="данный метод используется для получения данных">GET</span> | <span title="данный метод используется для изменения данных">PUT</span> | <span title="данный метод используется для удаления данных">DELETE</span> </b></p>
+		   	<br>	
+		   	<div class="mini-text"><p>наведи что бы узнать о методе</p></div>
+    	</section>
+    </div>
+
 	<div class="content">
-		
-		<div class="crud">
-			blur - window
-			form here
-			routes
-			send post 
+		<div class="backg-filter content-wrapper">
+			<div class="crud">
+				blur - window
+				form here
+				routes
+				send post 
+			</div>
 		</div>
-
-
 	</div>
+
 	<footer>
 		
 	</footer>
